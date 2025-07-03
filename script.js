91b7ea68f6f93e36aaf5edea6386385288cb749c
Yashagx <ya8009672@gmail.com>
2025-07-03 19:23:36 +0530

    FINAL

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navbar links
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
